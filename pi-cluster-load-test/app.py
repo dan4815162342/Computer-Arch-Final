6bd37a3d89714bf6c8dfa33c450fbfeeaa0fe947
dan4815162342 <danielhein28@gmail.com>
2025-11-14 12:12:33 -0500

    Changed pi-cluster load server

--- pi-cluster-load-test/app.py
+++ pi-cluster-load-test/app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # A simple, fast response for the load test
-    return "Hello from the Pi Load Test Server!"
+    # Simple Test to see if Webserver is working
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
